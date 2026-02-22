af26f8b27bfb323cb9b668abf3ec5af596737a17
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-02-22 22:38:58 +0300

    create fastapi app

--- social-backend/src/social_backend/app.py
+++ social-backend/src/social_backend/app.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI
+from dishka import make_async_container
+from dishka.integrations.fastapi import setup_dishka
 
-app = FastAPI(
-    title="Social Backend",
-)
+from social_backend.config import settings
+from social_backend.controllers.views import router
+from social_backend.ioc import AppProvider
+
+container = make_async_container(AppProvider())
 
 
-@app.get("")
-def hello_world() -> dict[str, str]:
-    return {"message": "Hello World"}
+def get_fastapi_app() -> FastAPI:
+    fastapi_app = FastAPI(
+        title=settings.app.title,
+    )
+
+    fastapi_app.include_router(router)
+
+    setup_dishka(container, fastapi_app)
+
+    return fastapi_app
+
+
+def get_app() -> FastAPI:
+    fastapi_app = get_fastapi_app()
+
+    return fastapi_app
+
+
+app = get_app()
