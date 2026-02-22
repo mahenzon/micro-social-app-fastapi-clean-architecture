21d5794fb52aa0a4716095ff399cce8202054e47
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-02-22 21:48:33 +0300

    create UserGateway

---
+++ social-backend/src/social_backend/infra/gateways/__init__.py
@@ -0,0 +1,6 @@
+__all__ = (
+    # Gateways
+    "UserGateway",
+)
+
+from .user_gateway import UserGateway
---
+++ social-backend/src/social_backend/infra/gateways/user_gateway.py
@@ -0,0 +1,28 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from social_backend.application.interfaces import UserReader, UserSaver
+from social_backend.domain import User, UserID
+from social_backend.infra import models
+
+
+class UserGateway(
+    UserReader,
+    UserSaver,
+):
+    def __init__(self, session: AsyncSession):
+        self._session = session
+
+    async def read_by_uuid(
+        self,
+        uuid: UserID,
+    ) -> User | None:
+        user = await self._session.get(models.User, uuid)
+        if user is None:
+            return None
+        return User.model_validate(user)
+
+    async def save(self, user: User) -> None:
+        user_model = models.User(**user.model_dump())
+        self._session.add(user_model)
+        await self._session.commit()
