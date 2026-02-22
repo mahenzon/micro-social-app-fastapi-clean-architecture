f257899c83a23f3ff91525166d302a7a2bdd5790
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-02-22 22:37:40 +0300

    allow model validate from attributes for domain model

--- social-backend/src/social_backend/domain/user.py
+++ social-backend/src/social_backend/domain/user.py
@@ -1,10 +1,14 @@
 from typing import TypeAlias
 from uuid import UUID
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 UserID: TypeAlias = UUID
 
 
 class User(BaseModel):
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
+
     id: UserID
     username: str
